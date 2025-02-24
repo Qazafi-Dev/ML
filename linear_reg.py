2c63f006af1b31dfde147397e479c3794696f0f8
FoolCoder <qazafi.sheikh121@gmail.com>
2025-02-24 19:29:19 +0000

    remove duplicate data

--- linear_reg.py
+++ linear_reg.py
@@ -5,14 +5,17 @@ import matplotlib.pyplot as plt
 from sklearn import preprocessing, svm 
 from sklearn.model_selection import train_test_split 
 from sklearn.linear_model import LinearRegression 
-df = pd.read_csv('energy.csv') 
-df_binary = df[['Country', 'Year']] 
+df = pd.read_csv('energy.csv',) 
+# print(df.head())
+df.columns = df.columns.to_series().apply(lambda x: x.strip())
 
-# Taking only the selected two attributes from the dataset 
-df_binary.columns = ['Con', 'Yrs'] 
-#display the first 5 rows 
+# print(df.columns)
+print('No. of records in dataset: ', len(df.index))
+print('No. of columns in dataset: ', len(df.columns))
+isNull=sum(df.isna().sum()) #checking null valus in data file and taking their sum
+print ('Total missing values in set', isNull)
+isDuplicate= df[df.duplicated(['Country', 'Year','Energy Type'])].sort_values('Year') #checking duplicate values in data file and taking their sum
+print ('Total duplicate values in set', len(isDuplicate.index))
 
-print(df_binary.head())
-#plotting the Scatter plot to check relationship between Sal and Temp 
-sns.lmplot(x ="Con", y ="Yrs", data = df_binary, order = 2, ci = None) 
-plt.show()
+
+
